bd6000767440a9a2aeb1972dcedaa9b2367ed25b
Ivo Stoyanov <ivostoy@gmail.com>
2021-02-05 06:35:36 -0800

    q

---
+++ work/hello_queue.py
@@ -0,0 +1,28 @@
+import queue
+import threading
+import time
+
+q = queue.Queue()
+
+
+def worker():
+    while True:
+        item = q.get()
+        print(f'Working on {item}')
+        time.sleep(0.5)
+        print(f'Finished {item}')
+        q.task_done()
+
+
+# turn-on the worker thread
+threading.Thread(target=worker, daemon=True).start()
+
+# send thirty task requests to the worker
+for item in range(10):
+    time.sleep(0.1)
+    q.put(item)
+print('All task requests sent\n', end='')
+
+# block until all tasks are done
+q.join()
+print('All work completed')
